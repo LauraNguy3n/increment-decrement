edf6cca72c7157508c938d8da34951a1357eb2dd
laura <you@example.com>
2024-08-20 12:42:53 -0500

    hallo :D!

---
+++ increment_decrement/src/increment_decrement/IncreDecre.java
@@ -0,0 +1,31 @@
+package increment_decrement;
+
+public class IncreDecre {
+
+	public static void main(String[] args) {
+		
+		/*+ addition
+		- subtraction
+		* multiplication
+		/ division
+		
+		++ increment--  the value of x will increase by 1
+		   preincrement ++x --> first the value will add then print
+		   postincrement x++ --> first the value will print as it is, then 1 will add
+		
+		-- decreament
+		   predecrement --x --> first the value will minus then print
+		   postdecrement x-- --> first the value will print as it is, then 1 will minus
+		   
+		   */
+		
+		int x;
+		  x=5;
+		  
+		  System.out.println(x--);
+		  System.out.println(x);
+
+		 
+	}
+
+}
